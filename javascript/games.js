01cec93f1682e55e5c1838342772d6eb40fab7e9
Heath Woodruff <happyheathbar@gmail.com>
2022-02-17 10:38:03 -0700

    removed brackets in declaration of content

--- javascript/games.js
+++ javascript/games.js
@@ -1,6 +1,6 @@
 document.getElementById("gameGriffin").addEventListener("click", function(event) {
 	event.preventDefault();
-	let content[] = document.getElementsByClassName("content");
+	let content = document.getElementsByClassName("content");
 	for (let i = 0; i < content.length; ++i) {
 		content[i].innerHTML = "";
 	}

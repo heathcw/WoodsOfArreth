c1b0da7344bc31046499b27a20934558f1805866
Heath Woodruff <happyheathbar@gmail.com>
2022-02-17 17:29:21 -0700

    fixing bug of +=

--- javascript/games.js
+++ javascript/games.js
@@ -1,7 +1,8 @@
 document.getElementById("gameGriffin").addEventListener("click", function(event) {
 	event.preventDefault();
 	document.querySelector(".content").innerHTML = "";
-	let newContent += "<div class='buttons'>";
+	let newContent = "<div class='buttons'>";
 	newContent += "<input id='joke' type='submit' value='Tell me a joke!'></input><br>";
 	newContent += "<input id='fact' type='submit' value='How are you?'></input><br>";
+	document.querySelector(".content").innerHTML = newContent;
 });

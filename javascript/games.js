3ccf90eb868ca075482b1e40e0b73c263bf424db
Heath Woodruff <happyheathbar@gmail.com>
2022-02-17 17:43:17 -0700

    forgot to close quotes

--- javascript/games.js
+++ javascript/games.js
@@ -4,6 +4,6 @@ document.getElementById("gameGriffin").addEventListener("click", function(event)
 	let newContent = "<div class='buttons'>";
 	newContent += "<input id='joke' type='submit' value='Tell me a joke!'></input><br>";
 	newContent += "<input id='fact' type='submit' value='How are you?'></input><br>";
-	newContent += "<input id='question' type='text' placeholder='Ask a yes or no question!'></input><input id='submit' type='submit' value='Ask'></input><br>;
+	newContent += "<input id='question' type='text' placeholder='Ask a yes or no question!'></input><input id='submit' type='submit' value='Ask'></input><br>";
 	document.querySelector(".content").innerHTML = newContent;
 });

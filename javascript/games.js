d11f70864e7c17289bcaf5a497f7d8534a916620
Heath Woodruff <happyheathbar@gmail.com>
2022-02-17 17:53:44 -0700

    made griffin image smaller and added griffin to the game

--- javascript/games.js
+++ javascript/games.js
@@ -5,5 +5,6 @@ document.getElementById("gameGriffin").addEventListener("click", function(event)
 	newContent += "<input id='joke' type='submit' value='Tell me a joke!'></input><br>";
 	newContent += "<input id='fact' type='submit' value='How are you?'></input><br>";
 	newContent += "<input id='question' type='text' placeholder='Ask a yes or no question!'></input><input id='submit' type='submit' value='Ask'></input><br>";
+	newContent += "</div><div class='griffin'><img src='https://easydrawingguides.com/wp-content/uploads/2020/06/Griffin-Step-10.png'></div>";
 	document.querySelector(".content").innerHTML = newContent;
 });

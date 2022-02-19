c616dfed25e9c066d9d380b6e4a44183a0e7188f
Heath Woodruff <happyheathbar@gmail.com>
2022-02-19 11:34:54 -0700

    trying with all api's, hoping that text of child will be replaced with each click

--- javascript/games.js
+++ javascript/games.js
@@ -7,18 +7,48 @@ document.getElementById("gameGriffin").addEventListener("click", function(event)
 	newContent += "<input id='question' type='text' placeholder='Ask a yes or no question!'></input><input id='submit' type='submit' value='Ask'></input><br>";
 	newContent += "</div><div class='griffin'><img src='https://easydrawingguides.com/wp-content/uploads/2020/06/Griffin-Step-10.png'></div>";
 	document.querySelector(".content").innerHTML = newContent;
+	let childNode = document.createElement("p");
+	document.querySelector(".content .griffin").appendChild(childNode);
 	let urlJoke = "https://geek-jokes.sameerkumar.website/api?format=json";
 	let urlFact = "https://asli-fun-fact-api.herokuapp.com/";
+	let urlSubmit = "https://zenquotes.io/api/random";
 	document.getElementById("joke").addEventListener("click", function(event) {
 		event.preventDefault();
 		fetch(urlJoke)
 		.then(function(response) {
 			return response.json();
 		}).then(function(json) {
-			let childNode = document.createElement("p");
-			let child = document.createTextNode(json.joke);
-			childNode.appendChild(child);
-			document.querySelector(".content .griffin").appendChild(childNode);
+			childNode.innerHTML = json.joke + " hahahahaha";
+		});
+	});
+	document.getElementById("fact").addEventListener("click", function(event) {
+		event.preventDefault();
+		fetch(urlFact)
+		.then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			childNode.innerHTML = "I'm doing great! Did you know that " + json.data.fact + "?";
+		});
+	});
+	document.getElementById("submit").addEventListener("click", function(event) {
+		event.preventDefault();
+		fetch(urlSubmit)
+		.then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			let random = Math.random(4);
+			if (random == 0) {
+				childNode.innerHTML = "No, " + json.q;
+			}
+			else if (random == 1) {
+				childNode.innerHTML = "Yes! " + json.q;
+			}
+			else if (random == 2) {
+				childNode.innerHTML = "Maybe... " + json.q;
+			}
+			else {
+				childNode.innerHTML = "Ask again later."
+			}
 		});
 	});
 });

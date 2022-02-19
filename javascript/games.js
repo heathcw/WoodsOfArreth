582f9a9e59a33c23802b613a31e7ffeea856b670
Heath Woodruff <happyheathbar@gmail.com>
2022-02-18 22:10:26 -0700

    calling joke api with json and appending joke as a child to griffin

--- javascript/games.js
+++ javascript/games.js
@@ -7,4 +7,16 @@ document.getElementById("gameGriffin").addEventListener("click", function(event)
 	newContent += "<input id='question' type='text' placeholder='Ask a yes or no question!'></input><input id='submit' type='submit' value='Ask'></input><br>";
 	newContent += "</div><div class='griffin'><img src='https://easydrawingguides.com/wp-content/uploads/2020/06/Griffin-Step-10.png'></div>";
 	document.querySelector(".content").innerHTML = newContent;
+	let urlJoke = "https://geek-jokes.sameerkumar.website/api?format=json";
+	let urlFact = "https://asli-fun-fact-api.herokuapp.com/";
+	document.getElementById("joke").addEventListener("click", function(event) {
+		event.preventDefault();
+		fetch(urlJoke)
+		.then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			let child = "<p>" + json.joke + "</p>";
+			document.querySelector(".griffin").appendChild(child);
+		});
+	});
 });

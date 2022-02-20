001b2819f3dd3502b8dedce6a028882a271086c3
Heath Woodruff <happyheathbar@gmail.com>
2022-02-19 22:18:02 -0700

    made griffin speak above griffin image and made griffin image smaller

--- javascript/games.js
+++ javascript/games.js
@@ -7,11 +7,14 @@ document.getElementById("gameGriffin").addEventListener("click", function(event)
 	let newContent = "<div class='buttons'>";
 	newContent += "<input id='joke' type='submit' value='Tell me a joke!'></input><br>";
 	newContent += "<input id='fact' type='submit' value='How are you?'></input><br>";
-	newContent += "<input id='question' type='text' placeholder='Ask a yes or no question!'></input><input id='submit' type='submit' value='Ask'></input><br>";
-	newContent += "</div><div class='griffin'><img src='https://easydrawingguides.com/wp-content/uploads/2020/06/Griffin-Step-10.png'></div>";
+	newContent += "<div><input id='question' type='text' placeholder='Ask a yes or no question!'></input><input id='submit' type='submit' value='Ask'></input></div><br>";
+	newContent += "</div><div class='griffin'></div>";
 	document.querySelector(".content").innerHTML = newContent;
 	let childNode = document.createElement("p");
+	let image = document.createElement("img");
+	image.src = 'https://easydrawingguides.com/wp-content/uploads/2020/06/Griffin-Step-10.png';
 	document.querySelector(".content .griffin").appendChild(childNode);
+	document.querySelector(".content .griffin").appendChild(image);
 	let urlJoke = "https://geek-jokes.sameerkumar.website/api?format=json";
 	let urlFact = "https://asli-fun-fact-api.herokuapp.com/";
 	let urlSubmit = "https://quoteclear.web.app/api/random";

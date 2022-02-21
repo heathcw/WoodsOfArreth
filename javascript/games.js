fefdcdfcc6bb4cb53106070e983805e7791db8b2
Heath Woodruff <happyheathbar@gmail.com>
2022-02-21 11:32:27 -0700

    added a second game called cave troll with nested selectors

--- javascript/games.js
+++ javascript/games.js
@@ -1,6 +1,10 @@
 function Random(max) {
 	return Math.floor(Math.random() * max);
 }
+function NextDecision(decisionA, decisionB) {
+	document.getElementById('a').innerHTML = "<input type='submit' value=" + decisionA + "></input>";
+	document.getElementById('b').innerHTML = "<input type='submit' value=" + decisionB + "></input>";
+}
 document.getElementById("gameGriffin").addEventListener("click", function(event) {
 	event.preventDefault();
 	document.querySelector(".content").innerHTML = "";
@@ -55,6 +59,49 @@ document.getElementById("gameGriffin").addEventListener("click", function(event)
 			else {
 				childNode.innerHTML = "Ask again later."
 			}
+		});
+	});
+});
+
+document.getElementById("gameTroll").addEventListener("click", function(event) {
+	event.preventDefault();
+	document.querySelector(".content").innerHTML = "";
+	let newContent = "<div class='game-box'><div class='gameEvents'><p>It's dark.<br>It's cold.<br>Do you...</p></div><div class='gameButtons'>"
+	newContent += "<div id='a'><input type='submit' value='A. Look for your flashlight?'></input></div>"
+	newContent += "<div id='b'><input type='submit' value='B. Walk forward?'></input></div></div></div>"
+	document.querySelector(".content").innerHTML = newContent;
+	document.getElementById('a').addEventListener("click", function(event) {
+		document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>No Flashlight.<br>It's getting warmer.<br>It's getting louder.<br>Do you...</p>";
+		NextDecision("Follow the noise?","Walk forward?");
+		document.getElementById('a').addEventListener("click", function(event) {
+			document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>It's hot.<br>It's breathing down your neck.<br>It got you.</p>";
+		});
+		document.getElementById('b').addEventListener("click", function(event) {
+			document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>There's a door.<br>The door is locked.<br>It's Hot.<br>Do you...</p>";
+                	NextDecision("Call for help?","Bang on the door?");
+			document.getElementById('a').addEventListener("click", function(event) {
+				document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>Nobody heard.<br>It covers your mouth.<br>It got you.</p>";
+			});
+			 document.getElementById('b').addEventListener("click", function(event) {
+				 document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>It's loud.<br>It's breathing down your neck.<br>It got you.</p>";
+			});
+		});
+	});
+	document.geyElementById('b').addEventListener("click",function(event) {
+		document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>There's a door.<br>The door is locked.<br>It's getting warmer.<br>Do you...</p>";
+                NextDecision("Check to the left for a key?","Bang on the door?");
+		document.getElementById('a').addEventListener("click", function(event) {
+			document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>It's hot.<br>It's breathing down your neck.<br>There's a key on the floor.<br>Do you...</p>";
+                	NextDecision("Unlock the door?","Turn around?");
+			document.getElementById('a').addEventListener("click", function(event) {
+				document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>You run out into a bright open field.<br>You let out a sigh of relief.<br>You are free.<br>But so is the It.</p>"
+			});
+			document.getElementById('b').addEventListener("click", function(event) {
+                                document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>It's horrifying.<br>You stab it with the key.<br>It screeches.<br>Your ears bleed.<br>It got you.</p>"
+			});
+		});
+		document.getElementById('b').addEventListener("click", function(event) {
+			document.querySelector(".content .game-box .gameEvents").innerHTML = "<p>It's loud.<br>It's breathing down your neck.<br>It got you.</p>";
 		});
 	});
 });
